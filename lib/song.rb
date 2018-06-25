f8ceb485a8ce0f3d41a8a2fc0ecd12be317cf3dc
Michael Blandford <mdblandford@gmail.com>
2018-06-25 19:59:36 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,57 +9,57 @@ class Song
   def save
     self.class.all << self
   end
-  
+
   def self.create
-    song = self.new 
+    song = self.new
     song.save
-    song 
+    song
   end
-  
+
   def self.new_by_name(name)
     song = self.new
     song.name = name
     song
   end
-  
-  def self.create_by_name(songName)
-    song = self.create  
-    song.name = songName
+
+  def self.create_by_name(name)
+    song = self.create
+    song.name = name
     song
   end
-  
-  def self.find_by_name(songName)
-    self.all.detect {|song| song.name == songName}
+
+  def self.find_by_name(name)
+    self.all.detect {|song| song.name == name}
   end
-  
+
   def self.find_or_create_by_name(songName)
     find_by_name(songName) || create_by_name(songName)
   end
-  
+
   def self.alphabetical
     self.all.sort_by {|song| song.name}
   end
-  
+
   def self.new_from_filename(filename)
     title = filename.split(" - ")
     artist = title[0]
     songName, extension = title[1].split(".")
-    song = self.new 
+    song = self.new
     song.artist_name = artist
     song.name = songName
     song
   end
-  
+
   def self.create_from_filename(filename)
     title = filename.split(" - ")
     artist = title[0]
     songName, extension = title[1].split(".")
-    song = self.create 
+    song = self.create
     song.artist_name = artist
     song.name = songName
     song
   end
-  
+
   def self.destroy_all
     @@all.clear
   end
